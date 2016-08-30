633922a0a73a7047a7365cc14bd2f3fc5a63aad4
Sergii Velykodnyi <sergii.velykodnyi@gmail.com>
2016-08-30 22:27:08 +0300

    Added ES2015 into the gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,31 @@
-const atImport      = require('postcss-import');
-const autoprefixer  = require('autoprefixer');
-const browserSync   = require('browser-sync').create();
-const concat        = require('gulp-concat-util');
-const cssnano       = require('gulp-cssnano');
-const del           = require('del');
-const gulp          = require('gulp');
-const gulpIf        = require('gulp-if');
-const imagemin      = require('gulp-imagemin');
-const jade          = require('gulp-jade');
-const plumber       = require('gulp-plumber');
-const postcss       = require('gulp-postcss');
-const sass          = require('gulp-sass');
-const sourcemaps    = require('gulp-sourcemaps');
-const uglify        = require('gulp-uglify');
+'use strict';
+
+import atImport from 'postcss-import';
+import autoprefixer from 'autoprefixer';
+import browserSync from 'browser-sync';
+import concat from 'gulp-concat-util';
+import cssnano  from 'gulp-cssnano';
+import del from 'del';
+import gulp from 'gulp';
+import gulpIf from 'gulp-if';
+import imagemin from 'gulp-imagemin';
+import jade from 'gulp-jade';
+import plumber from 'gulp-plumber';
+import postcss from 'gulp-postcss';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import uglify from 'gulp-uglify';
 
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 // Paths to the source code.
 const sourcePath = 'source';
 const sourcePaths = {
-  html:     sourcePath + '/*.jade',
-  icons:    sourcePath + '/icons/*.css',
-  images:   sourcePath + '/images/**/*.{png,jpg,svg}',
-  scripts:  sourcePath + '/scripts/**/*.js',
-  styles:   sourcePath + '/styles/**/*.scss'
+  html:     `${sourcePath}/*.jade`,
+  icons:    `${sourcePath}/icons/*.css`,
+  images:   `${sourcePath}/images/**/*.{png,jpg,svg}`,
+  scripts:  `${sourcePath}/scripts/**/*.js`,
+  styles:   `${sourcePath}/styles/**/*.scss`
 };
 
 // Path to the application
@@ -44,26 +46,26 @@ const imageminOption = {
 };
 
 // Task: html
-gulp.task('html', () => {
+gulp.task('html', () =>
   gulp.src(sourcePaths.html)
     .pipe(plumber())
     .pipe(jade({
       pretty: isDev ? true : false
     }))
-    .pipe(gulp.dest(buildPath));
-});
+    .pipe(gulp.dest(buildPath))
+);
 
 // Task: scripts.
-gulp.task('scripts', () => {
+gulp.task('scripts', () =>
   gulp.src(sourcePaths.scripts)
     .pipe(plumber())
     .pipe(concat('main.js'))
     .pipe(gulpIf(!isDev, uglify()))
-    .pipe(gulp.dest(buildPath + '/scripts'));
-});
+    .pipe(gulp.dest(`${buildPath}/scripts`))
+);
 
 // Task: styles.
-gulp.task('styles', () => {
+gulp.task('styles', () =>
   gulp.src(sourcePaths.styles)
     .pipe(plumber())
     .pipe(gulpIf(isDev, sourcemaps.init()))
@@ -74,36 +76,36 @@ gulp.task('styles', () => {
     ]))
     .pipe(gulpIf(!isDev, cssnano()))
     .pipe(gulpIf(isDev, sourcemaps.write()))
-    .pipe(gulp.dest(buildPath + '/styles'))
-    .pipe(browserSync.stream());
-});
+    .pipe(gulp.dest(`${buildPath}/styles`))
+    .pipe(browserSync.stream())
+);
 
 // Task: Images.
-gulp.task('images', () => {
+gulp.task('images', () =>
   gulp.src(sourcePaths.images)
     .pipe(imagemin(imageminOption))
-    .pipe(gulp.dest(buildPath + '/images'))
-    .pipe(browserSync.stream());
-});
+    .pipe(gulp.dest(`${buildPath}/images`))
+    .pipe(browserSync.stream())
+);
 
 // Task: Clean
 gulp.task('clean', () => del(buildPath));
 
 // Task: watch
-gulp.task('watch', ['default', 'server'], function() {
+gulp.task('watch', ['default', 'server'], () => {
   gulp.watch(sourcePaths.styles, ['styles']);
   gulp.watch(sourcePaths.images, ['images']);
   gulp.watch(sourcePaths.scripts, ['scripts']).on('change', browserSync.reload);
-  gulp.watch(sourcePath + '/**/*.{jade,md}', ['html']);
+  gulp.watch(`${sourcePath}/**/*.{jade,md}`, ['html']);
 });
 
 // Task: server
-gulp.task('server', () => {
+gulp.task('server', () =>
   browserSync.init({
-    files: buildPath + '/**/*.html',
+    files: `${buildPath}/**/*.html`,
     server: buildPath
-  });
-});
+  })
+);
 
 // Task: default
 gulp.task('default', ['html', 'styles', 'scripts', 'images']);
